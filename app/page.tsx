b301de5fec08ad6d66554e9ad63a503146b4c4c4
Aaron Tse <tse16@purdue.edu>
2024-08-22 00:47:41 -0400

    changed colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <main className = "relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className = "max-w-7xl w-full">
-        <h1>Hello, this is the start of the greatest portfolio of all time fr</h1>
+        <Hero />
       </div>
     </main>
   );
