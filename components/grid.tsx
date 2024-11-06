46ccd05212074d6a7f9ad70851c2ed8dbf78696a
Aaron Tse <tse16@purdue.edu>
2024-11-06 02:05:41 -0500

    made first steps of "bento grid"

---
+++ components/grid.tsx
@@ -0,0 +1,21 @@
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+
+const grid = () => {
+  return (
+    <section id="about">
+        <BentoGrid>
+            {[{ title: 'Title1', description: 'Desc1', id: 1}].map
+            ((item) => (
+                <BentoGridItem
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                />
+            ))}
+        </BentoGrid>
+    </section>
+  )
+}
+
+export default grid
