6d85966fb2283f159bae00067b752b621032235f
Aaron Tse <tse16@purdue.edu>
2024-11-07 00:22:50 -0500

    wtf idek what is going on at this point

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Grid from '@/components/Grid_idk';
+import Grid from '@/components/Grid_idkk';
 import Hero from '@/components/Hero';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import { FaHome } from 'react-icons/fa';
